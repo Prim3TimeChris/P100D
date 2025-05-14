b12746f3027887a826a5d674605952862e073c98
Christian <chrishermonstyneb1z@outlook.com>
2025-05-14 17:12:58 -0400

    Add files via upload
    Band Generator using the print f function instead

---
+++ ProjectClone/Band Gen.py
@@ -0,0 +1,11 @@
+# this will be band name generator using print f
+
+print("welcome to the band name Generator")
+
+SName = input("what is the name of the Street you grew up on ")
+
+PName = input("What what your favorite animal growing up ")
+
+Shell = SName  + " "  + PName
+
+print(f"Your Band Name will be {Shell}")
